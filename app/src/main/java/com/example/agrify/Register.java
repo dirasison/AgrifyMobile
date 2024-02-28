3638cafedaa7b012011fc46b1c7a0f40dac5d6fc
Arthan Kyle Job E. Ydeo <146556757+dirasison@users.noreply.github.com>
2024-02-28 19:38:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/agrify/Register.java
@@ -0,0 +1,27 @@
+package com.example.agrify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+        Button button = findViewById(R.id.btnRegister);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Register.this, Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
